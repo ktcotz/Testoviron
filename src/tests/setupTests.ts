452c2843380d48112a12e74382855538239f61b4
ktcotz <naskret.kamil@gmail.com>
2024-11-04 20:35:02 +0100

    feat: msw
    Add MSW with setup and configuration.

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,8 +1,14 @@
-import { afterEach } from "vitest";
+import { afterAll, afterEach, beforeAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
+import { server } from "../mocks/node";
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
+  server.resetHandlers();
 });
+
+beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
+
+afterAll(() => server.close());
