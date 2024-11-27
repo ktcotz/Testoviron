16b78c82fbed816f03226694ac27cde2c772f193
ktcotz <naskret.kamil@gmail.com>
2024-11-27 18:29:36 +0100

    feat: advanced playwright
    Work with advanced playwright components testing.

--- src/tests/e2e/general/generalLearning.spec.ts
+++ src/tests/e2e/general/generalLearning.spec.ts
@@ -81,4 +81,50 @@ test.describe("General learning testing of components", () => {
 
     await deleteButton.click();
   });
+
+  test("Sliders testing", async ({ page }) => {
+    await page.goto("http://localhost:4200/");
+
+    const tempGauge = page.locator(
+      `[tabtitle="Temperature"] ngx-temperature-dragger circle`
+    );
+
+    // Manual
+
+    await tempGauge.evaluate((node) => {
+      node.setAttribute("cx", "232.630");
+      node.setAttribute("cy", "232.630");
+    });
+
+    await tempGauge.click();
+
+    // Mouse automation
+
+    const tempBox = page.locator(
+      `[tabtitle="Temperature"] ngx-temperature-dragger`
+    );
+
+    await tempBox.scrollIntoViewIfNeeded();
+
+    const box = await tempBox.boundingBox();
+
+    const x = box.x + box!.width / 2;
+    const y = box.y + box!.height / 2;
+
+    await page.mouse.move(x, y);
+    await page.mouse.down();
+
+    await page.mouse.move(x + 100, y);
+    await page.mouse.move(x + 100, y + 100);
+    await page.mouse.up();
+
+    await expect(tempBox).toHaveText(/30/);
+  });
+  test("Drag&Drop", async ({ page }) => {
+    await page.goto("https://globalsqa.com/demo-site/draganddrop/");
+
+    const frame = page.frameLocator(`[rel-title="Photo Manager"] iframe`);
+
+    await frame.locator("li", { hasText: "High Tatras 2" }).click();
+  });
 });
